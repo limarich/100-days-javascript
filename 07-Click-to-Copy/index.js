a80dff5714c9a95615768f8ebb744ba8627c7f57
Richard <richard.esclima@gmail.com>
2022-06-13 02:09:05 -0300

    update copy method

--- 07-Click-to-Copy/index.js
+++ 07-Click-to-Copy/index.js
@@ -3,8 +3,10 @@ const coupon = document.querySelector(".coupon");
 
 btn.addEventListener("click", (e) => {
     e.preventDefault();
-    coupon.select();
-    coupon.setSelectionRange(0, 9999);
-    document.execCommand("copy");
-    btn.innerHTML = "Copied";
+    navigator.clipboard.writeText(coupon.value).then(() => {
+        btn.innerHTML = "Copied";
+        setTimeout(() => {
+            btn.innerHTML = "Copy";
+        }, 3000);
+    })
 });
